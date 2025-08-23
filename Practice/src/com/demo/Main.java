b5d735f57d5a1ec9b496c92635fa9aec203ca5fd
Md Sohail <csitsohail@gmail.com>
2025-08-23 16:24:52 +0530

    23rdAug2025

--- Practice/src/com/demo/Main.java
+++ Practice/src/com/demo/Main.java
@@ -1,6 +1,8 @@
 package com.demo;
 
 import java.util.Scanner;
+
+
 /*
 Create flowchart and pseudocode for the following:
 Input a year and find whether it is a leap year or not.
@@ -11,18 +13,25 @@ Keep taking numbers as inputs till the user enters ‘x’, after that print sum
 */
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Pls Provide the number");
-        while (true) {
-            int num = scanner.nextInt();
 
-            char userEnter = scanner.next().toLowerCase().trim().charAt(0);
-            if (userEnter=='x'){
-                break;
-            }
+
+    public static void main(String[] args) {
+        for (int i = 0; i <1000; i++) {
+            arm(i);
         }
     }
+        static void arm ( int val) {
+            int num = val;
+            int news = 0;
+            while (num > 0) {
+                int rem = num % 10;
+                rem = rem * rem * rem;
+                news = news + rem;
+                num = num / 10;
+            }
+            if (news==val){
+            System.out.println(news);
+        }}
 }
 
 
--- README.md
+++ README.md
@@ -7,11 +7,11 @@
 7. Take name as input and print a greeting message for that particular name.
 8. Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
 9. Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
-10.Take 2 numbers as input and print the largest number.
-11.Input currency in rupees and output in USD.
-12.
-13.
-14.
+10. Take 2 numbers as input and print the largest number.
+11. Input currency in rupees and output in USD.
+12. To calculate Fibonacci Series up to n numbers.
+13. To find out whether the given String is Palindrome or not.
+14. To find Armstrong Number between two given number.
 15.
 16.
 17.
